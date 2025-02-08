b0e247438619a2e9c6d7d048be3dadb825603b2f
Tozik Yury <urijtozik@gmail.com>
2025-02-08 17:27:20 +0300

    Суммирование чисел

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main() {
+	int a, b,summ;
+	cout << "Enetr number one" << endl;
+	cin >> a;
+	cout << "Enter number two" << endl;
+	cin >> b;
+	summ = a + b;
+	cout << "Summa:" << summ << endl;
+	return 0;
+}
