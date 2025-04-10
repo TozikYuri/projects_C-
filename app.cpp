1a7cc5f2474022239eabc911146785b03576e908
Tozik Yury <urijtozik@gmail.com>
2025-04-10 20:12:48 +0300

    Пустой проект

---
+++ app.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+
+using namespace std;
+
+
+
+int main() {
+
+	int sizeArray;
+
+	//int[] ** array;
+
+	setlocale(LC_ALL,"ru");
+
+	cout << "Размерность массива" << endl;
+
+	return 0;
+}
