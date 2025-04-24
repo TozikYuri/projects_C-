0dbba3354a134575a7c2500a9420b83169cfe0da
Tozik Yury <urijtozik@gmail.com>
2025-04-24 14:26:13 +0300

    Работа с массивами

--- app.cpp
+++ app.cpp
@@ -4,20 +4,29 @@ using namespace std;
 
 int main() {
 
-  	int numberOne, numberTwo;
-	int summa;
-
 	setlocale(LC_ALL,"ru");
+	
+	float sum=0;
 
-	cout << "Enter number one " << endl;
-	cin>> numberOne ;
-	cout << "Enter number two" << endl;
-	cin >> numberTwo;
+	float max = 0;
+	float min = 0;
 
-	if (numberOne == numberTwo) cout << "1=2" << endl;
-	else if (numberOne > numberTwo)cout << "1>2" << endl;
-	else cout << "2>1" << endl;
+	float array[] = {12.3,4.5,-5.3,7.8,12.5,6.8,-2.6};
 
+	cout << "Исходный массив" << endl;
+
+	for (float n : array) {
+		cout << n << " ";
+		if (n < 0)sum += n;
+		if (n > max)max = n;
+		if (n < min)min = n;
+	}
+	cout << "\nСумма отрицательных элементов массива:"<<sum<< endl;
+	cout << "Max:" << max << endl;
+	cout << "Min" << min << endl;
+	
 
 	return 0;
 }
+
+
